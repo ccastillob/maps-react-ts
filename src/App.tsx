0b82d767bf17800504b848d43cdbfcd54ff232ba
Christofer Castillo <ccastillo993a@gmail.com>
2022-12-23 23:15:53 -0500

    style: se aplica el formato establecido en App y main

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
     <div className="App">
-     <h1>Hola Mundo</h1>
+      <h1>Hola Mundo</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
